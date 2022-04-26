a4beca6ef57189bf28bc9ffd4c6bfb2e2bfbc404
Barbe <barbe.anthony@gmail.com>
2022-04-26 14:51:45 +0200

    dependencies MariaDB et EL dans pom.xml

--- src/main/java/servlets/Servlet_Create_Account.java
+++ src/main/java/servlets/Servlet_Create_Account.java
@@ -9,10 +9,23 @@ import java.io.IOException;
 public class Servlet_Create_Account extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO récupère infos du formulaire de création de compte
+        // récupère infos du formulaire de création de compte : username, password, email
         //TODO criptage du mdp (chercher une bibliothèque Java qui le fait)
         //TODO création du nouvel utilisateur dans la BDD
         //TODO renvoie à my_account.jsp en étant connecté, pour ajout du premier animal
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        HttpSession session = request.getSession();
+
+        /* Verification de pseudo et email disponibles:
+        sinon : renvoi à page de connexion
+        si ok : enregistrement dans bdd
+         */
+        session.setAttribute("username", username);
+
+        //renvoi à la page de gestion du compte
+        this.getServletContext().getRequestDispatcher("../index.jsp").forward(request, response);
     }
 
     @Override
