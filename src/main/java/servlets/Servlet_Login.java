45325d17b2e6bfb67e5a3b2d4ad7d51c07183698
Etienne <jacques.etienne057@hotmail.fr>
2022-04-26 14:24:29 +0200

    Début Login, servlet+cookie

--- src/main/java/servlets/Servlet_Login.java
+++ src/main/java/servlets/Servlet_Login.java
@@ -7,16 +7,37 @@ import java.io.IOException;
 
 @WebServlet(name = "Servlet_Login", value = "/Servlet_Login")
 public class Servlet_Login extends HttpServlet {
+
+
+    private static void setCookie( HttpServletResponse response, String nom, String valeur, int maxAge ) {
+
+        Cookie cookie = new Cookie( nom, valeur );
+        cookie.setMaxAge( maxAge );
+        response.addCookie( cookie );
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO Récupère les identifiants et MdPs depuis formulaire dans user_login.jsp
         //TODO cryptage du mdp saisi pour comparer avec le mdp de la BDD (si mdp enregistrés cryptés)
+
         //TODO Création de cookie de connexion
         //TODO Renvoie vers index.jsp en étant connecté, ou vers user_login.jsp avec message d'erreur si identifiants incorrects
+
+        final int COOKIE_MAX_AGE = 60 *60 *24 *365;
+        String email = (String)request.getParameter("email");
+        HttpSession session = request.getSession();
+
+        session.setAttribute("email", email);
+        setCookie(response,"email",email,COOKIE_MAX_AGE);
+
+        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
+
 }
--- src/main/webapp/jsp_files/user_login.jsp
+++ src/main/webapp/jsp_files/user_login.jsp
@@ -11,10 +11,15 @@
     <title>Login</title>
 </head>
 <body>
-
-
-
-
+<h1>Login</h1>
+<br>
+<form action="../Servlet_Login" method="post">
+    <label for="email"> Saisir une adresse email :</label> <input type="email" id="email" name="email">
+    <br>
+    <label for="password"> Saisir un mot de passe :</label> <input type="password" id="password" name="password">
+    <br>
+    <input type="submit" value="valider">
+</form>
 
 </body>
 </html>
