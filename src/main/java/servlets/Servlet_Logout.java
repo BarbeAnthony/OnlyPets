3b389eceba06878da4db2878e3e36f7b048d6bbc
Etienne <jacques.etienne057@hotmail.fr>
2022-04-26 15:20:34 +0200

    login avancement

--- src/main/java/servlets/Servlet_Logout.java
+++ src/main/java/servlets/Servlet_Logout.java
@@ -7,13 +7,29 @@ import java.io.IOException;
 
 @WebServlet(name = "Servlet_Logout", value = "/Servlet_Logout")
 public class Servlet_Logout extends HttpServlet {
+
+    private static void setCookie( HttpServletResponse response, String nom, String valeur, int maxAge ) {
+
+        Cookie cookie = new Cookie( nom, valeur );
+        cookie.setMaxAge( maxAge );
+        response.addCookie( cookie );
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        setCookie(response,"nemail","lamort",0);
+
+        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 }
