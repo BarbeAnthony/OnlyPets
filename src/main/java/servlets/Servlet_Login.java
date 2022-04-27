093d857d16277157f7fe7b146d5d81a29e8db06d
Etienne <jacques.etienne057@hotmail.fr>
2022-04-27 14:51:30 +0200

    Modifs appel jsp vers servet, new user/login

--- src/main/java/servlets/Servlet_Login.java
+++ src/main/java/servlets/Servlet_Login.java
@@ -20,15 +20,21 @@ public class Servlet_Login extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO Récupère les identifiants et MdPs depuis formulaire dans user_login.jsp
+
         //TODO cryptage du mdp saisi pour comparer avec le mdp de la BDD (si mdp enregistrés cryptés)
 
-        //TODO Création de cookie de connexion
-        //TODO Renvoie vers index.jsp en étant connecté, ou vers user_login.jsp avec message d'erreur si identifiants incorrects
 
         final int COOKIE_MAX_AGE = 60 *60 *24 *365;
-        String email = (String)request.getParameter("email");
-        String password = request.getParameter("password");
+
+
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+
+       if(email == null && password == null){
+           this.getServletContext().getRequestDispatcher("/jsp_files/user_login.jsp").forward(request, response);
+           return;
+       }
+
         HttpSession session = request.getSession();
         String username = "";
 
